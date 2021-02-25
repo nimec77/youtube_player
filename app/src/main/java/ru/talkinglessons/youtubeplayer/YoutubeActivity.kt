367286c5a5863723c8bf536bd96220dac6991996
comrade77 <nimec77@gmail.com>
2021-02-25 17:01:55 +0700

    Закончил №3

--- app/src/main/java/ru/talkinglessons/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/ru/talkinglessons/youtubeplayer/YoutubeActivity.kt
@@ -1,14 +1,33 @@
 package ru.talkinglessons.youtubeplayer
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+//        setContentView(R.layout.activity_youtube)
+//        val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+//        button1.text = getString(R.string.buttonAdded)
+//        layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(
